31e986ed812b892186055047e537c8b6a92263be
Rakshan22 <100563149+Rakshan22@users.noreply.github.com>
2022-07-18 11:39:58 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,16 @@
+monthConversions = {
+    0: "January",
+    1: "Feburary",
+    2: "March",
+    10: "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions.get(10))
